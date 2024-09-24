66afcd0f6988953e51b6449ad5730fe47cc91147
Mayur Patil <mayur.patil@aurelia.tech>
2024-09-25 05:29:41 +0530

    Fixing Docker

--- backend/models/User.js
+++ backend/models/User.js
@@ -38,8 +38,12 @@ UserSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
         return next();
     }
-    //const salt = await bcrypt.genSalt(10);
-    //this.password = await bcrypt.hash(this.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    console.log(`Updating password${this.password} and salt: ${salt}`);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    console.log(`Hashed password: ${hashedPassword}`);
+    this.password = hashedPassword;
+    console.log(this.password);
     next();
 });
 
