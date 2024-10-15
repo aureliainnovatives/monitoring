22742859d6a575673d6e0cd3ee63b2213fb45950
Mayur Patil <mayur.patil@aurelia.tech>
2024-10-16 01:45:08 +0530

    Minor changes on landing page

--- landing/build.js
+++ landing/build.js
@@ -5,20 +5,28 @@ const config = require('./config');
 const env = process.env.NODE_ENV || 'development';
 const envConfig = config[env];
 
-const inputFile = path.join(__dirname, 'index.html');
-const outputFile = path.join(__dirname, 'dist', 'index.html');
+const inputFiles = ['index.html', 'contact.html'];
+const inputDir = __dirname;
+const outputDir = path.join(__dirname, 'dist');
 
-let htmlContent = fs.readFileSync(inputFile, 'utf8');
-
-Object.keys(envConfig).forEach(key => {
-  const regex = new RegExp(`{{${key}}}`, 'g');
-  htmlContent = htmlContent.replace(regex, envConfig[key]);
-});
-
-if (!fs.existsSync(path.dirname(outputFile))) {
-  fs.mkdirSync(path.dirname(outputFile), { recursive: true });
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, { recursive: true });
 }
 
-fs.writeFileSync(outputFile, htmlContent);
+inputFiles.forEach(filename => {
+  const inputFile = path.join(inputDir, filename);
+  const outputFile = path.join(outputDir, filename);
+
+  let htmlContent = fs.readFileSync(inputFile, 'utf8');
+
+  Object.keys(envConfig).forEach(key => {
+    const regex = new RegExp(`{{${key}}}`, 'g');
+    htmlContent = htmlContent.replace(regex, envConfig[key]);
+  });
+
+  fs.writeFileSync(outputFile, htmlContent);
+  console.log(`Built ${env} version of ${filename}`);
+});
+ 
 
 console.log(`Built ${env} version of index.html`);
