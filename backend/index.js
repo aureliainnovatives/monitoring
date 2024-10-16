6d17c14f75b0a210388ca9846d8b7fe3ae111e64
Mayur Patil <mayur.patil@aurelia.tech>
2024-10-16 10:07:42 +0530

    Changed Assignment Service batch size to 1000

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,12 @@ const keywordAssignmentService = require('./services/KeywordAssignmentService');
 
 require('dotenv').config();
 
+// Serve static files from the 'assets' directory
+app.use(express.static('assets'));
+
+// If you want to specify a virtual path prefix for your static directory, you can do:
+// app.use('/static', express.static('assets'));
+
 
 // Middleware
 app.use(morgan('dev')); // Logging
